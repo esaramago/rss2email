ada30e40e6122be5ba9f1cd8ddae7dbc6bf8b08b
Emanuel Saramago <dev@emanuelsaramago.com>
2026-06-23 23:09:32 +0100

    update layout

--- index.js
+++ index.js
@@ -29,10 +29,13 @@ class RSS2Email {
 
     const parser = new Parser()
 
-    let postsHTML = '' 
+    let articlesHTML = '' 
 
     for (const feed of this.rssFeeds) {
       const feedData = await parser.parseURL(feed.url)
+
+      articlesHTML += `<h2 style="font-size: 30px; margin-bottom: 30px;">${feedData.title}</h2>`
+
       const thisWeekFeed = feedData.items.filter(item => {
         const pubDate = new Date(item.pubDate)
         const now = new Date()
@@ -40,35 +43,44 @@ class RSS2Email {
         return pubDate.getTime() >= oneWeekAgo.getTime()
       })
 
-      postsHTML += `<h2>${feed.name}</h2>`
-
       if (thisWeekFeed.length > 0) {
-        postsHTML += `
-          <ul>
-            ${thisWeekFeed.map(item => `
-              <li>
-                <time>${new Date(item.pubDate).toLocaleDateString()}</time>
-                <a href="${item.link}">${item.title}</a>
-              </li>
-            `).join('')}
-          </ul>
+        articlesHTML += `
+          ${thisWeekFeed.map(item => `
+            <article style="margin-bottom: 40px;">
+              <time>${new Date(item.pubDate).toLocaleDateString()}</time>
+              <a href="${item.link}" style="color: #007bff; text-decoration: underline;">
+                <h3 style="margin: 0; font-size: 24px;">${item.title}</h3>
+              </a>
+              ${item.contentSnippet ? `<p>${item.contentSnippet}</p>` : ''}
+            </article>
+          `).join('')}
         `
       } else {
-        postsHTML += `<p>No new posts this week.</p>`
+        articlesHTML += `<p>Não há artigos novos nesta semana.</p>`
       }
     }
 
-    const dates = `From ${new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000).toLocaleDateString()} to ${new Date().toLocaleDateString()}`
+    const startDate = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000)
+    const endDate = new Date()
+
+    const content = `
+      <div style="font-family: sans-serif; font-size: 16px; color: #333; width: 100%; max-width: 600px; margin: 0 auto; padding: 20px;">
+        Aqui estão os artigos da última semana das fontes RSS:
+        <section>
+          ${articlesHTML}
+        </section>
+      </div>
+    `.replaceAll('\n', '')
 
     await this.#sendEmail(
-      `RSS4Email - ${dates}`,
-      `Here are the latest posts from your RSS feeds:\n\n${postsHTML}`
+      `Artigos de ${startDate.getDate()} ${startDate.toLocaleString('default', { month: 'long' })} a ${endDate.getDate()} ${endDate.toLocaleString('default', { month: 'long' })}`,
+      content
     )
   }
 
   async #sendEmail(subject, text) {
     await this.transporter.sendMail({
-      from: `"RSS Bot" <${process.env.SMTP_USER}>`,
+      from: `"RSS2Email" <${process.env.EMAIL_FROM}>`,
       to: process.env.EMAIL_TO,
       subject,
       text,
@@ -78,9 +90,10 @@ class RSS2Email {
   }
 }
 
-
-// Executa de hora a hora (ou ajusta no Coolify)
-//schedule('0 * * * *', () => rss2Email.main())
-
 const rss2Email = new RSS2Email()
-rss2Email.main()
+
+// Execute once a day
+schedule('0 0 * * *', () => rss2Email.main())
+
+// Test execution
+// rss2Email.main()
