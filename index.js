208c6acdb6a6ef0bfea0428ae4d470eb74966653
Emanuel Saramago <dev@emanuelsaramago.com>
2026-06-24 16:45:35 +0100

    run Test execution only in development

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import RSS2Email from './src/main.js'
 import { schedule } from 'node-cron'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const rss2Email = new RSS2Email()
 
+if (process.env.ENV === 'development') {
+  // Test execution
+  rss2Email.init()
+}
+
 // Execute once a day
 schedule('0 0 * * *', () => rss2Email.init())
-
-// Test execution
-rss2Email.init()
