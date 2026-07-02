12d8b0964e854c6c6b7d2fe56fde9165539b327a
Emanuel Saramago <dev@emanuelsaramago.com>
2026-07-02 15:02:05 +0100

    more index.js to src

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import RSS2Email from './src/main.js'
+import RSS2Email from './main.js'
 import { schedule } from 'node-cron'
 import dotenv from 'dotenv'
 import { readFile } from 'fs/promises'
 
 dotenv.config()
 
-const feedsList = JSON.parse(await readFile(new URL('./src/feeds.json', import.meta.url), 'utf8'))
-const settings = JSON.parse(await readFile(new URL('./src/settings.json', import.meta.url), 'utf8'))
+const feedsList = JSON.parse(await readFile(new URL('./feeds.json', import.meta.url), 'utf8'))
+const settings = JSON.parse(await readFile(new URL('./settings.json', import.meta.url), 'utf8'))
 
 const rss2Email = new RSS2Email(feedsList,settings)
 if (process.env.ENV === 'development') {
